df4071f39a6a9dc956a81cf4cb1fe8431f834577
Алексей Бельченков <u608110@gmail.com>
2022-01-22 11:35:48 +0400

    Android currentHeight for layout

--- App.js
+++ App.js
@@ -1,10 +1,32 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar as ExpoStatusBar } from 'expo-status-bar';
+import {
+    StyleSheet,
+    SafeAreaView,
+    Text,
+    View,
+    Platform,
+    StatusBar,
+} from 'react-native';
 
 export default function App() {
   return (
-    <View>
-    </View>
+    <>
+        <SafeAreaView
+            style={{ flex: 1, marginTop: StatusBar.currentHeight }}
+        >
+            <View
+                style={{ padding: 16, backgroundColor: 'green' }}
+            >
+                <Text>search</Text>
+            </View>
+            <View
+                style={{ flex: 1, padding: 16, backgroundColor: 'blue' }}
+            >
+                <Text>list</Text>
+            </View>
+        </SafeAreaView>
+        <ExpoStatusBar style="auto" />
+    </>
   );
 }
 
