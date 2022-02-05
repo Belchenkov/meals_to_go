86efcff9631de438605d8b4554b7faea924eb065
Алексей Бельченков <u608110@gmail.com>
2022-02-05 13:31:49 +0400

    Migrate restaurants.screen on styled components

--- src/features/restaurants/screens/restaurants.screen.js
+++ src/features/restaurants/screens/restaurants.screen.js
@@ -1,34 +1,36 @@
 import React from 'react';
-import { SafeAreaView, StatusBar, StyleSheet, View } from 'react-native';
+import { SafeAreaView, StatusBar, View } from 'react-native';
 import { Searchbar } from 'react-native-paper';
+import styled from 'styled-components/native';
 
 import RestaurantInfoCard from '../components/restaurant-info-card.component';
 
+const SafeArea = styled(SafeAreaView)`
+    flex: 1;
+    margin-top: ${StatusBar.currentHeight}px;
+`;
+
+const SearchContainer = styled(View)`
+    padding: 16px;
+`;
+
+const RestaurantListContainer = styled(View)`
+    flex: 1;
+    padding: 16px;
+    background-color: white;
+`;
+
 const RestaurantsScreen = () => {
     return (
-        <SafeAreaView style={styles.container}>
-            <View style={styles.search}>
+        <SafeArea>
+            <SearchContainer>
                 <Searchbar />
-            </View>
-            <View style={styles.list}>
-            <RestaurantInfoCard />
-            </View>
-        </SafeAreaView>
+            </SearchContainer>
+            <RestaurantListContainer>
+                <RestaurantInfoCard />
+            </RestaurantListContainer>
+        </SafeArea>
     );
 };
-
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        marginTop: StatusBar.currentHeight,
-    },
-    search: {
-        padding: 16,
-    },
-    list: {
-        flex: 1,
-        padding: 16,
-    },
-});
 
 export default RestaurantsScreen;
