47b58b4c88887e17fdafbfdb37d2520cd15e5242
Алексей Бельченков <u608110@gmail.com>
2022-03-05 13:58:33 +0400

    Cleanup restaurant-info styles

--- src/features/restaurants/components/restaurant-info-card.component.js
+++ src/features/restaurants/components/restaurant-info-card.component.js
@@ -1,49 +1,20 @@
 import React from 'react';
-import { Card } from 'react-native-paper';
-import styled from 'styled-components/native';
 import { SvgXml } from 'react-native-svg';
 
 import star from '../../../../assets/star';
 import open from '../../../../assets/open';
 import { Spacer } from '../../../components/spacer/spacer.component';
 import { Text } from '../../../components/typography/text.component';
-import { Icon } from './restaurant-info-card.styles';
-
-const RestaurantCard = styled(Card)`
-    background-color: ${(props) => props.theme.colors.bg.primary};
-`;
-
-const RestaurantCardCover = styled(Card.Cover)`
-    padding: ${(props) => props.theme.space[3]};
-    background-color: ${(props) => props.theme.colors.bg.primary};
-`;
-
-const Info = styled.View`
-    padding: ${(props) => props.theme.space[3]};
-`;
-
-const Rating = styled.View`
-    flex-direction: row;
-    padding-top: ${(props) => props.theme.space[2]};
-    padding-bottom: ${(props) => props.theme.space[2]};
-`;
-
-const Address = styled.Text`
-    font-family: ${(props) => props.theme.fonts.body};
-    font-size: ${(props) => props.theme.fontSizes.caption};
-`;
-
-const Section = styled.View`
-    flex-direction: row;
-    align-items: center;
-`;
-
-const SectionEnd = styled.View`
-    flex: 1;
-    flex-direction: row;
-    align-items: center;
-    justify-content: flex-end;
-`;
+import {
+    Icon,
+    RestaurantCard,
+    RestaurantCardCover,
+    Info,
+    Section,
+    Rating,
+    SectionEnd,
+    Address,
+} from './restaurant-info-card.styles';
 
 const RestaurantInfoCard = ({ restaurant = {} }) => {
     const {
