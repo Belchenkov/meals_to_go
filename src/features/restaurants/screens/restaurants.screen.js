98376bdb2ca8de8ab325513b0098406750a54141
Алексей Бельченков <u608110@gmail.com>
2022-03-12 12:18:42 +0400

    No Inline Style

--- src/features/restaurants/screens/restaurants.screen.js
+++ src/features/restaurants/screens/restaurants.screen.js
@@ -15,13 +15,19 @@ const SearchContainer = styled(View)`
     padding: ${(props) => props.theme.space[3]};
 `;
 
+const RestaurantList = styled(FlatList).attrs({
+    contentContainerStyle: {
+        padding: 16,
+    },
+})``;
+
 const RestaurantsScreen = () => {
     return (
         <SafeArea>
             <SearchContainer>
                 <Searchbar />
             </SearchContainer>
-            <FlatList
+            <RestaurantList
                 data={[
                     { name: 1 },
                     { name: 2 },
