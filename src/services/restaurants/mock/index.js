b731441a228f5795abed4b756918d7a9559c8ff1
Алексей Бельченков <u608110@gmail.com>
2022-03-26 17:01:15 +0400

    Fake an api request

--- src/services/restaurants/mock/index.js
+++ src/services/restaurants/mock/index.js
@@ -7,7 +7,7 @@ export const mocks = {
   "51.219448,4.402464": antwerp,
   "43.653225,-79.383186": toronto,
   "41.878113,-87.629799": chicago,
-  "37.7749295,-122.4194155": san_francisco,
+ "37.7749295,-122.4194155" : san_francisco,
 };
 
 export const mockImages = [
