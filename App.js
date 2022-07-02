8a0617ddc4b91625b6858f2637d030d22fcac854
Алексей Бельченков <u608110@gmail.com>
2022-07-02 19:14:59 +0400

    Install Firebase

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { StatusBar as ExpoStatusBar } from 'expo-status-bar';
 import { ThemeProvider } from 'styled-components/native';
+import * as firebase from 'firebase';
 import {
     useFonts as useOswald,
     Oswald_400Regular,
@@ -12,6 +13,17 @@ import { RestaurantsContextProvider } from './src/services/restaurants/restauran
 import { LocationContextProvider } from './src/services/location/location.context';
 import { FavouritesContextProvider } from './src/services/favourites/favourites.context';
 import Navigation from './src/infrastructure/navigation';
+
+const firebaseConfig = {
+    apiKey: 'AIzaSyCnAeo3Wd1OCIZ64SEf-tvjhBxulHJwzWU',
+    authDomain: 'mealstogo-2e427.firebaseapp.com',
+    projectId: 'mealstogo-2e427',
+    storageBucket: 'mealstogo-2e427.appspot.com',
+    messagingSenderId: '982852923117',
+    appId: '1:982852923117:web:d10531dd77b4b1d8daa6e4',
+};
+
+firebase.initializeApp(firebaseConfig);
 
 export default function App() {
     const [oswaldLoaded] = useOswald({
