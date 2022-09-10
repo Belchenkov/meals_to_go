173635ff347e90ec0b3d9d23090243ea44fbcb96
Алексей Бельченков <u608110@gmail.com>
2022-09-10 14:36:20 +0400

    Account Screen Buttons

--- src/features/account/screens/account.screen.js
+++ src/features/account/screens/account.screen.js
@@ -1,11 +1,37 @@
 import React from 'react';
 
-import { AccountBackground, AccountCover } from '../components/account.styles';
+import {
+    AccountBackground,
+    AccountContainer,
+    AccountCover,
+    AuthButton,
+} from '../components/account.styles';
+import { Spacer } from '../../../components/spacer/spacer.component';
 
-const AccountScreen = () => {
+const AccountScreen = ({ navigation }) => {
     return (
         <AccountBackground>
             <AccountCover />
+            <AccountContainer>
+                <AuthButton
+                    icon="login"
+                    mode="contained"
+                    color="teal"
+                    onPress={() => navigation.navigate('Login')}
+                >
+                    Login
+                </AuthButton>
+                <Spacer size="large">
+                    <AuthButton
+                        icon="shield-account"
+                        mode="contained"
+                        color="maroon"
+                        onPress={() => navigation.navigate('Register')}
+                    >
+                        Register
+                    </AuthButton>
+                </Spacer>
+            </AccountContainer>
         </AccountBackground>
     );
 };
