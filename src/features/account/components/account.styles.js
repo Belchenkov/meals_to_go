9d804dc4fbedfef62ebd986c3260fb367f977530
Алексей Бельченков <u608110@gmail.com>
2022-09-10 15:52:13 +0400

    Login Screen

--- src/features/account/components/account.styles.js
+++ src/features/account/components/account.styles.js
@@ -1,7 +1,5 @@
 import styled from 'styled-components/native';
-import { Button } from 'react-native-paper';
-
-import { colors } from '../../../infrastructure/theme/colors';
+import { Button, TextInput } from 'react-native-paper';
 
 export const AccountBackground = styled.ImageBackground.attrs({
     source: require('../../../../assets/home_bg.jpg'),
@@ -26,4 +24,8 @@ export const AccountContainer = styled.View`
 
 export const AuthButton = styled(Button).attrs()`
     padding: ${(props) => props.theme.space[2]};
+`;
+
+export const AuthInput = styled(TextInput)`
+    width: 300px;
 `;
