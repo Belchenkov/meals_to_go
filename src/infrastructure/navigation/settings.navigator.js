3e2515e8dff9189ccf924093d3f95f8214284605
Алексей Бельченков <u608110@gmail.com>
2022-10-15 21:31:02 +0400

    Settings Feature

---
+++ src/infrastructure/navigation/settings.navigator.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import {
+    createStackNavigator,
+    CardStyleInterpolators,
+} from '@react-navigation/stack';
+
+import SettingsScreen from '../../features/settings/screens/settings.screen';
+
+const SettingsStack = createStackNavigator();
+
+const SettingsNavigator = ({ route, navigation }) => {
+    return (
+        <SettingsStack.Navigator
+            headerMode="screen"
+            screenOptions={{
+                cardStyleInterpolator: CardStyleInterpolators.forHorizontalIOS,
+            }}
+        >
+            <SettingsStack.Screen
+                options={{
+                    header: () => null,
+                }}
+                name="Settings"
+                component={SettingsScreen}
+            />
+            <SettingsStack.Screen name="Favourites" component={() => null} />
+        </SettingsStack.Navigator>
+    );
+};
+
+export default SettingsNavigator;
