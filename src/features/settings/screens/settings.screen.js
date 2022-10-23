edbc28ec1a127ad27bfa211f1f0aab6e40fda79a
Алексей Бельченков <u608110@gmail.com>
2022-10-23 14:42:17 +0400

    Making settings screen look nice

--- src/features/settings/screens/settings.screen.js
+++ src/features/settings/screens/settings.screen.js
@@ -1,17 +1,37 @@
 import React, { useContext } from 'react';
-import { List } from 'react-native-paper';
+import { Avatar, List } from 'react-native-paper';
+import styled from 'styled-components/native';
 
 import { SafeArea } from '../../../components/utility/safe-area.component';
 import { AuthContext } from '../../../services/auth/auth.context';
+import { Text } from '../../../components/typography/text.component';
+import { Spacer } from '../../../components/spacer/spacer.component';
+
+const SettingsItem = styled(List.Item)`
+    padding: ${(props) => props.theme.space[3]};
+`;
+
+const AvatarContainer = styled.View`
+    align-items: center;
+`;
 
 const SettingsScreen = ({ navigation }) => {
-    const { onLogout } = useContext(AuthContext);
+    const { onLogout, user } = useContext(AuthContext);
 
     return (
         <SafeArea>
+            <AvatarContainer>
+                <Avatar.Icon
+                    size={150}
+                    icon="human"
+                    backgroundColor="#2182BD"
+                />
+                <Spacer position="top" size="large">
+                    <Text variant="caption">{user.email}</Text>
+                </Spacer>
+            </AvatarContainer>
             <List.Section>
-                <List.Item
-                    style={{ padding: 16 }}
+                <SettingsItem
                     title="Favourites"
                     description="View your favourites"
                     left={(props) => (
@@ -19,8 +39,7 @@ const SettingsScreen = ({ navigation }) => {
                     )}
                     onPress={() => navigation.navigate('Favourites')}
                 />
-                <List.Item
-                    style={{ padding: 16 }}
+                <SettingsItem
                     title="Logout"
                     left={(props) => (
                         <List.Icon {...props} color="black" icon="door" />
