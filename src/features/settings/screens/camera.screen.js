bf094b0cda98309026308170f309a03a98d259cb
Алексей Бельченков <u608110@gmail.com>
2022-11-05 16:22:28 +0400

    allow permission camera

--- src/features/settings/screens/camera.screen.js
+++ src/features/settings/screens/camera.screen.js
@@ -1,7 +1,40 @@
-import React from 'react';
+import React, { useRef, useState, useEffect } from 'react';
+import { Camera } from 'expo-camera';
+import styled from 'styled-components/native';
+import { View } from 'react-native';
+
+import Text from '../../../components/typography/text.component';
+
+const ProfileCamera = styled(Camera)`
+    width: 100%;
+    height: 100%;
+`;
 
 const CameraScreen = () => {
-    return null;
+    const [hasPermission, setHasPermission] = useState(null);
+    const cameraRef = useRef();
+
+    useEffect(() => {
+        (async () => {
+            const { status } = await Camera.requestCameraPermissionsAsync();
+            setHasPermission(status === 'granted');
+        })();
+    });
+
+    if (hasPermission === null) {
+        return <View />;
+    }
+
+    if (hasPermission === false) {
+        return <Text>No access to camera</Text>;
+    }
+
+    return (
+        <ProfileCamera
+            ref={(camera) => (cameraRef.current = camera)}
+            type={Camera.Constants.Type.front}
+        />
+    );
 };
 
 export default CameraScreen;
